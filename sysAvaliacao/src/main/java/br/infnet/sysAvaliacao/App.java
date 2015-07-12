63e4cd5d368f22e4c5fe582430cff51b6b5ba1cb
antoniovilmar <antoniovilmar.castro@gmail.com>
2015-07-12 13:22:19 -0300

    Criação Project sysAvaliacao

---
+++ sysAvaliacao/src/main/java/br/infnet/sysAvaliacao/App.java
@@ -0,0 +1,13 @@
+package br.infnet.sysAvaliacao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
